270ad5d74f4d9f8aba15413efbe63da22d848907
qiminghao <qiminghao@heyhomi.com>
2019-08-20 17:32:02 +0800

    添加nacos config

---
+++ uaa/src/main/java/com/homismart/uaa/control/Test.java
@@ -0,0 +1,20 @@
+package com.homismart.uaa.control;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class Test {
+    @Value("${cwswx}")
+    private String cswxs;
+
+    @GetMapping("/getHello")
+    public String getHello() {
+        log.info("调动feign");
+        return cswxs;
+    }
+}
